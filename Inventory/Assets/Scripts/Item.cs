1e714b113e57a24e7bd354e0d2ca2a0e6e42134a
alejandroninsausti <alejandroninsausti@gmail.com>
2021-06-09 03:48:22 -0300

    Fix Background on Data Visualizer

--- Inventory/Assets/Scripts/Item.cs
+++ Inventory/Assets/Scripts/Item.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item")]
 public class Item : ScriptableObject
 {
-    [Serializable] public enum Type { armor, weapon, empty };
-    [Serializable] public enum Rarity { common, rare, veryRare, epic, legendary, empty}
+    public enum Type { armor, weapon, empty };
+    public enum Rarity { common, rare, veryRare, epic, legendary, empty}
 
     public Sprite itemImage;
-    [NonSerialized] public Sprite backgroundImage;
+    public Sprite backgroundImage;
     public string Name;
     public string description;
     public int price;
