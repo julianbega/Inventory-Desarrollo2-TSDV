2a7b2f6b05928efbec5a4744ba3a2739610260ae
Julian Bega <julian.begamilrad@gmail.com>
2021-06-08 22:18:15 -0300

    Intento guardar Player en playerPref

---
+++ Inventory/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Player myPlayer;
+    void Start()
+    {
+        myPlayer = FindObjectOfType<Player>();
+    }
+
+    public static void SaveInfo(Player myPlayer)
+    {
+        string json = JsonUtility.ToJson(myPlayer);
+        PlayerPrefs.SetString("PlayerInfo",json);
+    }
+
+    public static Player LoadInfo()
+    {        
+        Player myPlayer = FindObjectOfType<Player>();
+        string jsonData = PlayerPrefs.GetString("PlayerInfo");
+        JsonUtility.FromJsonOverwrite(jsonData, myPlayer);
+        return myPlayer;
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            SaveInfo(myPlayer);
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            myPlayer = LoadInfo();
+        }
+    }
+}
